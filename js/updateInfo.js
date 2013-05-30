401c46bdd96904ea928d17068a2753138bf63afb
Tyler Longren <tlongren@gmail.com>
2013-05-30 15:30:42 -0500

    Change again.

--- js/updateInfo.js
+++ js/updateInfo.js
@@ -1,13 +1,14 @@
-
-	$(document).setInterval(function(){
+$(document).ready(function() {
+	setInterval(function(){
 		theData = new Array();
 		$.ajax({
 		    url:"/updateInfo.php",
 		    type:"GET",
 		    success:function(result){
 		        theData = result;
-		        alert(result);
+		        alert(theData);
 		    },
 		    "json"
 		});
 	}, 5000);
+});	
