6001b0dc0ab1ed1a82ec3124b177990378eede94
Tyler Longren <tlongren@gmail.com>
2013-05-30 16:18:01 -0500

    Setup variables with status values.

--- js/updateInfo.js
+++ js/updateInfo.js
@@ -4,7 +4,19 @@ $(function() {
       function(result) {
 		//alert(result);
 		var serverStatus = result[0];
-		alert(serverStatus);
+		var totalMem = result[1];
+		var usedMem = result[2];
+		var availMem = result[3];
+		var memPercent = result[4];
+		var totalDisk = result[5];
+		var usedDisk = result[6];
+		var availDisk = result[7];
+		var diskPercent = result[8];
+		var totalBW = result[9];
+		var usedBW = result[10];
+		var availBW = result[11];
+		var bwPercent = result[12];
+		//alert(serverStatus);
     });
   }
   setInterval(loadVPSInfo, 600000);
