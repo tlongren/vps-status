c6ae882926a4762675c4c11912283ff0591d7655
tlongren <tlongren@gmail.com>
2014-02-21 17:03:27 -0600

    Generic config.

--- config.php
+++ config.php
@@ -1,16 +1,16 @@
 <?php
 /* You can edit all this stuff. */
-$apiurl = "https://solus.fliphost.net/api/client";							// URL to your API client URL, provided by your host
-$apikey = "MBU8J-OXTP9-6FM70";												// Your API Key, provided by your host
-$apihash = "09c999cb43e981f5eb4bcaacdf26e335948bb890";						// Your API Hash, provided by your host
-$dynamicUpdates = true;														// Poll the API every 5 seconds, set to false if you don't want this feature
-$refreshRate = "1000";														// Time in milliseconds before refreshing values from API
-$trustedIP = array("173.22.40.33","209.234.71.242");											// Allow these IP addresses
-$onlineMessage = "This is good, very good.";								// Message below "It's Online" text
-$onlineReload = "It's Good, Reload Anyway?";								// Text for online reload button
+$apiurl = "https://solus.fliphost.net/api/client";		                    // URL to your API client URL, provided by your host
+$apikey = "XXX11-XXX11-XXX11";												                    // Your API Key, provided by your host
+$apihash = "09c999cb43e981f5eb4bcaacdf26e335948bb890";						        // Your API Hash, provided by your host
+$dynamicUpdates = true;														                        // Poll the API every 5 seconds, set to false if you don't want this feature
+$refreshRate = "1000";														                        // Time in milliseconds before refreshing values from API
+$trustedIP = array();											                                // Allow these IP addresses
+$onlineMessage = "This is good, very good.";								              // Message below "It's Online" text
+$onlineReload = "It's Good, Reload Anyway?";								              // Text for online reload button
 $offlineMessage = "Something must be wrong, this is not good at all.";		// Message below "It's offline" text
-$offlineReload = "Try Again?";												// Text for offline reload button
-$theShortURL = 'http://goo.gl/rxZiN';										// URL that will be shared when people click share on twitter of google+ buttons
+$offlineReload = "Try Again?";												                    // Text for offline reload button
+$theShortURL = 'http://goo.gl/JZjs8V';										                // URL that will be shared when people click share on twitter of google+ buttons
 /* You should probably stop editing now. Only edit stuff after this if you know what you're doing. */
 
 include 'thestuff.php';
